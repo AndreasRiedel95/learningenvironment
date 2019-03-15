cf223ff9d70d6e11d4a6db157f0a131680d9b438
Andreas Riedel <ar113@hdm-stuttgart.de>
2019-03-15 19:42:33 +0100

    search on admin forms 2

--- src/js/admin/admin.js
+++ src/js/admin/admin.js
@@ -4,16 +4,24 @@ function searchList() {
 	filter = input.value.toUpperCase();
 	label = document.querySelectorAll('.list-value');
 
+	let lastLines = document.querySelectorAll('.--without-break')
+	lastLines.forEach((lastline) => {
+		lastline.classList.remove('--without-break')
+	})
 
 	// Loop through all list items, and hide those who don't match the search query
 	for (i = 0; i < label.length; i++) {
 		txtValue = label[i].innerHTML;
 		if (txtValue.toUpperCase().indexOf(filter) > -1) {
-			label[i].parentNode.style.display = ""; 
+			label[i].parentNode.classList.remove('--not-in-query') 
 		} else {
-			label[i].parentNode.style.display = "none";
+			label[i].parentNode.classList.add('--not-in-query') 
 		}
 		
+		let inQuery = document.querySelectorAll('.checkbox-group:not(.--not-in-query)');
+		if(inQuery !== null) {
+			inQuery[inQuery.length - 1].classList.add('--without-break')
+		}
 	}
 }
 
